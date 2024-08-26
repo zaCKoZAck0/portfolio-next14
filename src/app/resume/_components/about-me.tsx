5b60fcf008106f135547fc6ba8587f8a304da6a4
zackozack <id.ayushkryadav@gmail.com>
2024-08-26 20:31:40 +0530

    refactor: Add time-based greeting in AboutMe component

--- src/app/resume/_components/about-me.tsx
+++ src/app/resume/_components/about-me.tsx
@@ -1,7 +1,52 @@
+import { MoonIcon, SunMedium, SunriseIcon, SunsetIcon } from "lucide-react";
 import { CopyableText } from "~/components/typography";
 import { getRelativeDate } from "~/lib/date";
+import { Greet } from "./greet";
 
 export function AboutMe() {
+    let time = new Date().toLocaleTimeString("en-In",{
+        timeZone: "Asia/Kolkata",
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true
+    });
+    const hour = Number(time.split(":")[0]);
+    const am = time.split(" ")[1];
+    const Time = () => {
+        if (am === "am") {
+            if(hour >= 5 && hour < 12) {
+                return {
+                    icon: ()=><SunriseIcon className="md:size-5 size-4" />,
+                    time: "Morning"
+                };
+            } else {
+                return {
+                    icon: ()=><MoonIcon className="md:size-5 size-4" />,
+                    time: "Night"
+                };
+            }
+        } else {
+            if (hour >= 12 && hour < 5) {
+                return {
+                    icon: ()=><SunMedium className="md:size-5 size-4" />,
+                    time: "Afternoon"
+                };
+            } else if (hour >= 5 && hour < 7) {
+                return {
+                    icon: ()=><SunsetIcon className="md:size-5 size-4" />,
+                    time: "Evening"
+                };
+            } else {
+                return {
+                    icon: ()=><MoonIcon className="md:size-5 size-4" />,
+                    time: "Night"
+                };
+            }
+        }
+    }
+
+
+    const Icon = Time();
     return <section>
         <div>
             <table className="md:text-lg text-xs">
@@ -35,8 +80,9 @@ export function AboutMe() {
                     <td className="text-muted-foreground md:pr-4">
                         Timezone:
                     </td>
-                    <td className="font-semibold">
-                        Asia/Kolkata (UTC+5:30)
+                    <td className="font-semibold flex items-center gap-2">
+                        Asia/Kolkata (UTC+5:30) - {time}
+                        <Greet icon={Icon.icon()} time={Icon.time} />
                     </td>
                 </tr>
                 <tr>
--- src/components/animated-icon.tsx
+++ src/components/animated-icon.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Copy, Check, Mail } from "lucide-react";
+import { Copy, Check, Mail, MessageCircle, Smile } from "lucide-react";
 
 export const CopySuccessIcon = () => (
     <div className="relative text-green-200">
@@ -31,6 +31,23 @@ export const MailSentSuccess = () => (
     animate={{ opacity: 1, transform: "translate(0%, -100%)", transition: { duration: 0.2, delay: 1 } }}
     >
     <Check className="h-5 w-5" />
+    </motion.div>
+    </div>
+);
+
+export const Greet = () => (
+    <div className="relative text-yellow-200">
+    <motion.div
+    initial={{ transform: "translateX(0%)", opacity: 1 }}
+    animate={{ transform: "translateX(-100%)", opacity: 0, transition: { duration: 0.2, delay: 1 } }}
+    >
+    <MessageCircle className="h-5 w-5" />
+    </motion.div>
+    <motion.div className="absolute"
+    initial={{ opacity: 0, transform: "translate(100%, -100%)" }}
+    animate={{ opacity: 1, transform: "translate(0%, -100%)", transition: { duration: 0.2, delay: 1 } }}
+    >
+    <Smile className="h-5 w-5" />
     </motion.div>
     </div>
 );
