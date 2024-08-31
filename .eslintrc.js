a4efe562cefdb97fe3b8ea53e9eecb0e5938e268
zackozack <id.ayushkryadav@gmail.com>
2024-08-31 22:20:14 +0530

    refactor: eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
   extends: [
-    "next",
-    "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
     // Add your custom ESLint rules here
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/no-require-imports": "off",
-    "react/prop-types": "warn",
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-require-imports': 'off',
+    'react/prop-types': 'off',
   },
 };
--- src/lib/send-mail.ts
+++ src/lib/send-mail.ts
@@ -1,23 +1,27 @@
-"use server";
-import { Resend } from 'resend';
+'use server';
+import { CreateEmailResponseSuccess, Resend } from 'resend';
 
 const key = process.env.RESEND_API_KEY;
 
 const resend = new Resend(key);
 
 type SendEmailProps = {
-    email: string;
-    subject: string;
-    template: any;
-}
+  email: string;
+  subject: string;
+  template: React.ReactNode;
+};
 
 type SendEmailResponse = {
-    errorMessage?: string;
-    error: boolean;
-    data?: any;
-}
+  errorMessage?: string;
+  error: boolean;
+  data?: CreateEmailResponseSuccess | null;
+};
 
-export async function SendEmail({ email, subject, template }: SendEmailProps): Promise<SendEmailResponse> {
+export async function SendEmail({
+  email,
+  subject,
+  template,
+}: SendEmailProps): Promise<SendEmailResponse> {
   const { data, error } = await resend.emails.send({
     from: 'ZACKOZACK <bot@zackozack.xyz>',
     to: [email, 'id.ayushkryadav@gmail.com'],
@@ -32,4 +36,4 @@ export async function SendEmail({ email, subject, template }: SendEmailProps): P
   } else {
     return { data, error: false };
   }
-};
+}
