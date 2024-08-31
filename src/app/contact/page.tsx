210bf5783eca6af79e4b9628316b1413802a288a
zackozack <id.ayushkryadav@gmail.com>
2024-08-31 22:29:15 +0530

    bug: form padding typo

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import { ContactForm } from './_components/contact-form';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Contact Me | zaCKoZAck',
+  title: 'Contact Me | zackozack',
   description: 'I am a fullstack software engineer who loves to build things for the web.',
 };
 
@@ -10,7 +10,7 @@ export default function Contact() {
   return (
     <main className="bg-background text-foreground md:container">
       <section className="flex h-[calc(100vh-48px)] flex-col items-center justify-center">
-        <div className="px-6s w-full max-w-3xl border-primary/50 py-4 md:border-2">
+        <div className="w-full max-w-3xl border-primary/50 px-6 py-4 md:border-2">
           <ContactForm />
         </div>
       </section>
