421389b3d2fe27987a4c57fbcffa7a9485651086
zackozack <id.ayushkryadav@gmail.com>
2024-09-04 06:31:02 +0530

    refactor: Remove blogs for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-import { Hero } from "./_components/hero";
-import type { Metadata } from 'next'
-import { A } from "~/components/typography";
+import { Hero } from './_components/hero';
+import type { Metadata } from 'next';
+import { A } from '~/components/typography';
 
 export const metadata: Metadata = {
   title: 'Portfolio | zaCKoZAck',
   description: 'I am a fullstack software engineer who loves to build things for the web.',
-}
+};
 
 export default function Home() {
   return (
-    <main className="bg-background text-foreground container">
-      <section className="flex flex-col items-center justify-center h-[calc(100vh-48px)]">
-      <p className="my-2 flex items-center gap-2">Read my blogs here <A href="/blogs">Blogs</A>
-      </p>
+    <main className="container bg-background text-foreground">
+      <section className="flex h-[calc(100vh-48px)] flex-col items-center justify-center">
+        {/* <p className="my-2 flex items-center gap-2">Read my blogs here <A href="/blogs">Blogs</A>
+      </p> */}
         <Hero />
       </section>
     </main>
