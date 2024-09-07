68035cb36e6e44ee4582515df6188274781051e4
zackozack <id.ayushkryadav@gmail.com>
2024-09-08 04:14:17 +0530

    feat: added projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,25 +1,11 @@
-import { H1 } from '~/components/typography'
-import {ProjectCard} from '~/components/project-card'
-import githubData from '../../../data/github-data.json'
-import { Metadata } from 'next'
+import { Metadata } from 'next';
+import { Projects } from './_components/projects';
 
 export const metadata: Metadata = {
-    title: 'Projects | zackozack',
-    description: 'I am a fullstack software engineer who loves to build things for the web.',
-  }
+  title: 'Projects | zackozack',
+  description: 'A list of projects I have worked on.',
+};
 
-export default function Projects(){
-    return <main className="flex justify-center container">
-    <section className="flex flex-col w-full max-w-3xl mt-10">
-        <H1>Projects</H1>
-        <div className='mt-4'>
-            {
-                githubData.data.user.topRepositories.nodes.slice(0,2).map((repo, index) => {
-                    return <ProjectCard key={index} {...repo} />
-                })
-            }
-        </div>
-        
-    </section>
-    </main>
+export default function ProjectsPage() {
+  return <Projects />;
 }
