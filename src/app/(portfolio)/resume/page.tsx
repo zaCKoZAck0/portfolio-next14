3a3c1c7523f8216b86508af71f64610a1d894269
zackozack <id.ayushkryadav@gmail.com>
2024-09-18 10:59:35 +0530

    Refactor: Update styling and layout in resume page

--- src/app/(portfolio)/resume/page.tsx
+++ src/app/(portfolio)/resume/page.tsx
@@ -1,17 +1,17 @@
-import { ResumeAccordion } from "./_components/resume-accordion";
-import type { Metadata } from 'next'
+import { ResumeAccordion } from './_components/resume-accordion';
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Resume | zaCKoZAck',
   description: 'I am a fullstack software engineer who loves to build things for the web.',
-}
+};
 
 export default function Resume() {
-    return (
-        <main className="bg-background text-foreground container">
-            <section className="flex flex-col items-center justify-center h-[calc(100vh-48px)]">
-                <ResumeAccordion />
-            </section>
-        </main>
-    );
+  return (
+    <main className="container bg-background text-foreground">
+      <section className="mt-24 flex h-[calc(100vh-48px)] flex-col items-center justify-center">
+        <ResumeAccordion />
+      </section>
+    </main>
+  );
 }
