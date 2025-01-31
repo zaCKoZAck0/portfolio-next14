7cdb849b7dd4ccb55319d78fde88e288978aaa7b
zackozack <id.ayushkryadav@gmail.com>
2025-01-31 15:58:52 +0530

    chore: remove unused code

--- src/app/(portfolio)/resume/_components/skeleton/accordion-content.tsx
+++
@@ -1,9 +0,0 @@
-import { Skeleton } from '~/components/ui/skeleton';
-
-export function AccordionContentSkeleton() {
-  return (
-    <div>
-      <Skeleton className="mt-2 h-56 w-full" />
-    </div>
-  );
-}
